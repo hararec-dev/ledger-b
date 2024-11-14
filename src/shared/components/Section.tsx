a0b4c2a6b112bb0706e40f6adbaa454477c14f19
hararec-dev <hararecdev@gmail.com>
2024-11-13 18:35:55 -0600

    build(icons): se configuran íconos para Android

--- src/shared/components/Section.tsx
+++ src/shared/components/Section.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from "react";
 import { StyleSheet, Text, useColorScheme, View } from "react-native";
 import { Colors } from "react-native/Libraries/NewAppScreen";
+import Octicons from 'react-native-vector-icons/Octicons';
 
 type SectionProps = PropsWithChildren<{
     title: string;
@@ -10,6 +11,8 @@ export const Section = ({ children, title }: SectionProps): React.JSX.Element =>
     const isDarkMode = useColorScheme() === 'dark';
     return (
         <View style={styles.sectionContainer}>
+            <Octicons name="check" size={24} color="green" />
+            <Octicons name="git-pull-request" size={24} color="green" />
             <Text
                 style={[
                     styles.sectionTitle,
